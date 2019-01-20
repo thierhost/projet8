b99e9aa1e8fc8ea8a2633922b2ca45bf103920ee
emmanuelfd <33237914+emmanuelfd@users.noreply.github.com>
2019-01-20 22:26:50 +0000

    core update

--- core/views.py
+++ core/views.py
@@ -2,10 +2,7 @@ from django.http import HttpResponse
 from django.template import loader
 
 
-def home1(request):
-    message = "Salut tout le monde !"
-    return HttpResponse(message)
 
 def home(request):
-    template = loader.get_template('core/footer.html')
+    template = loader.get_template('core/accueil.html')
     return HttpResponse(template.render(request=request))
